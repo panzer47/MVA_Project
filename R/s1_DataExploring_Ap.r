d690ff6df97485cb175a01baf37b9135827b1f53
Apolo Ro <apolitogaga@gmail.com>
2015-05-21 20:08:11 +0200

    Initializing Part 2 of the document.
    Add a file in which I will work in, add a column for each of the
    variables in which we have missing values for.

---
+++ R/s1_DataExploring_Ap.r
@@ -0,0 +1,48 @@
+#After loading the file in the other document we continue with this file.
+unkValues <- adult[workclass==" ?",]
+names(adult)
+for(1:dim(unkValues))
+
+
+#> names(adult)
+#[1] "age"            "workclass"      "fnlwgt"         "education"      "education.num"  "martial-status"
+#[7] "occupation"     "relationship"   "race"           "sex"            "capital-gain"   "capital-loss"  
+#[13] "hours-per-week" "native-country" "income"        
+adult.p1 <- adult[1:2]
+adult.p2 <- adult[3:length(adult)]
+var = 0
+for( i in 1:length(workclass)
+     )
+  {
+  #print(i)
+  # very very very slow haha couldn't find another way :(
+  if(workclass[i] ==" ?")  
+    var[i] = TRUE
+  else
+    var[i] = FALSE
+}
+var <- as.factor(var)
+var <- as.data.frame(var)
+colnames(var) <- c("noWorkClass")
+adult <- cbind(adult.p1,var,adult.p2)
+
+adult.p1 <- adult[1:8]
+adult.p2 <- adult[9:length(adult)]
+var = 0
+for( i in 1:length(occupation)
+)
+{
+  #print(i)
+  # very very very slow haha couldn't find another way :(
+  if(occupation[i] ==" ?")  
+    var[i] = TRUE
+  else
+    var[i] = FALSE
+}
+table(adult$occupation[i] ==" ?")
+var <- as.factor(var)
+var <- as.data.frame(var)
+colnames(var) <- c("noOccupation")
+adult <- cbind(adult.p1,var,adult.p2)
+summary(adult$noOccupation)
+names(adult)
